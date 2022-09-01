b8f44032f968ac5d772c006e2126da10a21b2177
lunaisnotaboy <her@mint.lgbt>
2022-09-01 08:09:56 -0400

    Add missing `scoped` attribute to `<style>` tag

--- app/frontend/ui/visitor.vue
+++ app/frontend/ui/visitor.vue
@@ -2,7 +2,7 @@
   <h1 class="heading">Hello, World!</h1>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .heading {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
